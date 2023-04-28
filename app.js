6bfbc243e466d80a4f068cf723acbe1340366a65
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-04-28 11:10:47 +0200

    Home Page css completed

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const nunjucks = require('nunjucks');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const indexRouter = require('./routes/index');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+});
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
