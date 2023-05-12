02402e4bcf7785fc9567483ed4f14818088530ec
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-05-12 11:01:45 +0200

    Start of Manga/Light Novel page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
 
 
 router.get('/', function (req, res, next) {
     res.render('index.njk', {
         title: 'Home Page',
+    });
+});
+router.get('/manga', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT sb26forum.*, sb26users.name FROM sb26forum JOIN sb26users ON sb26forum.authorId = sb26users.id ORDER BY id DESC;");
+    res.render('manga.njk', {
+        rows: rows,
+        title: 'Manga'
     });
 });
 
